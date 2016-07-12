1ea0ea4092d972dc4e40cdde70cee02ab95a04f4
shashankm <shashank.shashi2@gmail.com>
2016-07-12 13:20:47 +0530

    Initial commit

---
+++ app/src/main/java/shashankm/circularrevealdemo/MainActivity.java
@@ -0,0 +1,24 @@
+package shashankm.circularrevealdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private FrameLayout container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        container = (FrameLayout) findViewById(R.id.container);
+
+        setLollipopAnimator();
+    }
+
+    private void setLollipopAnimator() {
+        LollipopSceneAnimator.newInstance(this, container, R.layout.scene1, R.layout.scene2, R.transition.arc1);
+    }
+}
